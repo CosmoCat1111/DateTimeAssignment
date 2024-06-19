458145c21e38a84290cdc18254dd540350fa3ccb
CosmoCat1111 <131853637+CosmoCat1111@users.noreply.github.com>
2024-06-19 11:20:01 -0700

    Add project files.

---
+++ DateTimeAssignment/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DateTimeAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+                DateTime currentDateTime = DateTime.Now;
+                Console.WriteLine("Current date and time: " + currentDateTime);
+
+                // user input
+                Console.Write("Enter your time: ");
+                string userInput = Console.ReadLine();
+
+                // convert
+                if (int.TryParse(userInput, out int hours))
+                {
+                    // calculate future time
+                    DateTime futureDateTime = currentDateTime.AddHours(hours);
+                    Console.WriteLine("In " + hours + " hours, it will be: " + futureDateTime);
+                }
+                else
+                {
+                    //handling
+                    Console.WriteLine("Whoops. Please try again.");
+                }
+
+                Console.WriteLine("\nPress any key to exit...");
+                Console.ReadKey();
+            }
+        }
+
+    }
+
